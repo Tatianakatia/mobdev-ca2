c7d3922c2ca241e25376d4516275608603874bd7
TKDutra <tatiana.katia+tati@gmail.com>
2020-04-22 23:18:15 +0000

    fixing favourite page

--- mobdev-ca2/src/app/pages/episodes/episodes.page.ts
+++ mobdev-ca2/src/app/pages/episodes/episodes.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-//import { NavController } from '@ionic/angular';
+
 import { Observable } from 'rxjs';
 import { ApiService } from '../../services/api.service';
 
