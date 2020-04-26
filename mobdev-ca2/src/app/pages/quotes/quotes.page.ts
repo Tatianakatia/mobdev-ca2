b269d7416d9b407fe10e5798d84ba4352231c79a
TKDutra <tatiana.katia+tati@gmail.com>
2020-04-26 10:40:11 +0000

    Search Quote

--- mobdev-ca2/src/app/pages/quotes/quotes.page.ts
+++ mobdev-ca2/src/app/pages/quotes/quotes.page.ts
@@ -3,44 +3,29 @@ import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { ApiService } from '../../services/api.service';
 
+ 
 @Component({
-    selector: 'app-quotes',
-    templateUrl: './quotes.page.html',
-    styleUrls: ['./quotes.page.scss'],
+  selector: 'app-quotes',
+  templateUrl: './quotes.page.html',
+  styleUrls: ['./quotes.page.scss'],
 })
 export class QuotesPage implements OnInit {
-
-    quotes: Observable<any>;
-    search: any = "";
-
-    constructor(
-        private router: Router,
-        private api: ApiService) { }
-
-    ngOnInit() {
-        this.quotes = this.api.getQuotes();
-        this.quotes.subscribe(data => {
-            console.log('my dataQuotes: ', data);
-        })
-
-    }
-
-
-    openDetails(quote) {
-    let quoteId = quote.quote_id;
-        this.router.navigateByUrl(`/tabs/quotes/${quote.quote_id}`);
-        console.log('my dataDeathdetails: ', quoteId);
-
-
-    }
-
-
+ 
+  quotes: Observable<any>;
+ 
+    searchTerm: any = "";
+  constructor(private router: Router, private api: ApiService) { }
+ 
+  ngOnInit() {
+    this.quotes = this.api.getQuotes();
+    this.quotes.subscribe(data => {
+      console.log('my data: ', data);
+      });
+  }
 
     searchQuote(quote) {
-        // console.log(death.author);
-        return quote.responsible.toLowerCase().indexOf(this.search.toLowerCase()) != -1;
-    }
+  
+          return quote.author.toLowerCase(quote).indexOf(this.searchTerm.toLowerCase()) != -1;
+      }
 
-    
-
-}
+ }
